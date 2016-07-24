847cf3c4566f6dbcf9460c0466117405aa3be9a6
narikim <wassupnari@gmail.com>
2016-07-24 14:17:38 -0700

    Connected TapGesture recognizer to SettingsViewController

--- SettingsViewController.swift
+++ SettingsViewController.swift
@@ -20,7 +20,10 @@ class SettingsViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 
     /*
     // MARK: - Navigation
